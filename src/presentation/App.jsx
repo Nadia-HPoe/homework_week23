588560b36fae0e248fc1fe4bad05c47d83a96a5d
Nadia-HPoe <isslandia@mail.ru>
2023-10-26 17:58:10 +0300

    work with states

--- src/presentation/App.jsx
+++ src/presentation/App.jsx
@@ -6,40 +6,30 @@ import ThemeGreen from './components/styles/rate-green.module.scss';
 import ThemeBlack from './components/styles/rate-black.module.scss';
 
 const rates = [
-  { id: 1, title: 300, price: 300, speed: 10 },
-  { id: 2, title: 450, price: 450, speed: 50 },
-  { id: 3, title: 550, price: 550, speed: 100, showBestChoice: true },
-  { id: 4, title: 1000, price: 1000, speed: 200 },
+  { id: 1, title: 300, price: 300, speed: 10, theme: ThemeBlue },
+  { id: 2, title: 450, price: 450, speed: 50, theme: ThemeGreen },
+  {
+    id: 3,
+    title: 550,
+    price: 550,
+    speed: 100,
+    theme: ThemeRed,
+  },
+  { id: 4, title: 1000, price: 1000, speed: 200, theme: ThemeBlack },
 ];
 
 function App() {
   return (
     <div className="App">
-      <Rate
-        title={rates[0].title}
-        price={rates[0].price}
-        speed={rates[0].speed}
-        theme={ThemeBlue}
-      ></Rate>
-      <Rate
-        title={rates[1].title}
-        price={rates[1].price}
-        speed={rates[1].speed}
-        theme={ThemeGreen}
-      ></Rate>
-      <Rate
-        title={rates[2].title}
-        price={rates[2].price}
-        speed={rates[2].speed}
-        theme={ThemeRed}
-        showBestChoice={rates[2].showBestChoice}
-      ></Rate>
-      <Rate
-        title={rates[3].title}
-        price={rates[3].price}
-        speed={rates[3].speed}
-        theme={ThemeBlack}
-      ></Rate>
+      {rates.map((rate) => (
+        <Rate
+          key={rate.id}
+          title={rate.title}
+          price={rate.price}
+          speed={rate.speed}
+          theme={rate.theme}
+        />
+      ))}
     </div>
   );
 }
--- src/presentation/Rate.jsx
+++ src/presentation/Rate.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './components/styles/rate.scss';
 
 function Rate(props) {
-  const { title, price, speed, theme, showBestChoice } = props;
+  const [showBestChoice, setPressed] = useState(false);
+  const handleChange = () => {
+    setPressed(!showBestChoice);
+  };
+  const { title, price, speed, theme } = props;
 
   return (
-    <div className={'card-body ' + (showBestChoice ? 'selected' : '')}>
+    <div
+      className={'card-body ' + (showBestChoice ? 'selected' : '')}
+      onClick={handleChange}
+    >
       <h4 className={theme.titlestyle}>Безлимитный {title}</h4>
       <p className={theme.textstyle}>руб {price}/мес</p>
       <span className="card-price">до {speed} Мбит/сек</span>
